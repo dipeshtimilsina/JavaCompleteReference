24308589ce9f06a9c3bd0d2a25e03e52f97e1c40
dipeshtech <dipesh.nepali@gmail.com>
2020-04-21 09:54:50 +0545

    Basic Class program

--- src/ClassBoxDemo.java
+++ src/ClassBoxDemo.java
@@ -1,3 +1,20 @@
+class Box{
+    double width;
+    double height;
+    double depth;
+}
 public class ClassBoxDemo {
-    
+    public static void main(String[] args) {
+        Box mybox= new Box();
+        double vol;
+
+        //assign values to mybox's instance variables
+
+        mybox.width= 10;
+        mybox.height= 4;
+        mybox.depth= 45;
+
+        vol= mybox.width* mybox.height* mybox.depth;
+        System.out.println("Volume is:" + vol);
+    }
 }

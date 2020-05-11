c2c3ac429effaaf3d6aaf7b56c4336bd759b2b2f
dipeshtech <dipesh.nepali@gmail.com>
2020-05-11 14:12:21 +0545

    Recursion Array Program

---
+++ src/methodclassdeep/RecursionArrayNumber.java
@@ -0,0 +1,31 @@
+package methodclassdeep;
+ class RecursionArray {
+    int values[];
+    RecursionArray (int i){
+        values= new int[i];
+    }
+
+    //display array recursively
+    void printArray (int i){
+        if (i==0) return;
+
+        else{
+            printArray(i-1);
+            System.out.println("["+ (i-1) + "] "+ values[i-1]);
+        }
+    }
+}
+ public class RecursionArrayNumber{
+     public static void main(String[] args) {
+         RecursionArray r = new RecursionArray(10);
+
+         int i;
+
+         for (i=0; i<10;i++){
+             r.values[i]=i;
+
+         }
+         r.printArray(10);
+     }
+ }
+

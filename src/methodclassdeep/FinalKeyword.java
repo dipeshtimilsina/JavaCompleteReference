d3f8ef1ed63ecda54d860d5217d373f8798754dd
dipeshtech <dipesh.nepali@gmail.com>
2020-05-12 13:23:28 +0545

    Final Keyword example

---
+++ src/methodclassdeep/FinalKeyword.java
@@ -0,0 +1,11 @@
+package methodclassdeep;
+
+public class FinalKeyword {
+    public static void main(String[] args) {
+        final int FILE_NEW= 1;
+        final int FILE_OPEN=2;
+        final int FILE_SAVE=3;
+        final int FILE_SAVEAS= 4;
+        final int FILE_QUIT=5;
+    }
+}

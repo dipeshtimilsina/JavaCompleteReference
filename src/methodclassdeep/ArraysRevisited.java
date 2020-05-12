0ff732df9ca8aef98a1a860476ac4fe6350d6908
dipeshtech <dipesh.nepali@gmail.com>
2020-05-12 13:42:02 +0545

    Final Keyword example

---
+++ src/methodclassdeep/ArraysRevisited.java
@@ -0,0 +1,13 @@
+package methodclassdeep;
+
+public class ArraysRevisited {
+    public static void main(String[] args) {
+        int[] a= new int[10];
+        int[] b= {1,2,3,4,5,6,7};
+        int[] c= {5,6,7,-10,01,4};
+
+        System.out.println(a.length);
+        System.out.println(b.length);
+        System.out.println(c.length);
+    }
+}

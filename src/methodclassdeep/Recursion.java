581a64fe6fdb8336c4b681d1f0b5b101250a3017
dipeshtech <dipesh.nepali@gmail.com>
2020-05-18 11:21:18 +0545

    Recursion revision

--- src/methodclassdeep/Recursion.java
+++ src/methodclassdeep/Recursion.java
@@ -1,22 +1,47 @@
 package methodclassdeep;
-
 class RecursionCode{
-    int fact (int n){
+    int recursion (int n){
         int result;
-        if (n==1)
+        if (n==1){
             return 1;
+        }
         else {
-            result = fact(n - 1) * n;
+            result = recursion(n - 1) * n;
             return result;
         }
     }
 }
 public class Recursion {
     public static void main(String[] args) {
-        RecursionCode test1= new RecursionCode();
-        System.out.println("The fact of 3 is: "+ test1.fact(3));
-        System.out.println("The fact of 5 is: "+ test1.fact(5));
-        System.out.println("The fact of 7 is: "+ test1.fact(7));
-
+        RecursionCode num1 = new RecursionCode();
+        RecursionCode num2 = new RecursionCode();
+        int a = num1.recursion(5);
+        int b = num2.recursion(10);
+        System.out.println("The factorial of a is " + a);
+        System.out.println("The factorial of b is " + b);
+        System.out.println("The factorial is: "+ num1.recursion(4));
     }
 }
+//package methodclassdeep;
+//
+//class RecursionCode{
+//    int fact (int n){
+//        int result;
+//        if (n==1)
+//            return 1;
+//        else {
+//            result = fact(n - 1) * n;
+//            return result;
+//        }
+//    }
+//}
+//public class Recursion {
+//    public static void main(String[] args) {
+//        RecursionCode test1= new RecursionCode();
+//        System.out.println("The fact of 3 is: "+ test1.fact(3));
+//        System.out.println("The fact of 5 is: "+ test1.fact(5));
+//        System.out.println("The fact of 7 is: "+ test1.fact(7));
+//
+//    }
+//}
+//

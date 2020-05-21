d80066b5e701c5aedcb5934b13bbcb70dafa9bab
dipeshtech <dipesh.nepali@gmail.com>
2020-05-21 13:08:24 +0545

    Varargs Ambiguouity

---
+++ src/methodclassdeep/VarargsAmbiguity.java
@@ -0,0 +1,34 @@
+package methodclassdeep;
+//varargs, overloading and ambiguity
+
+//this program will not compile and throw out an error
+
+public class VarargsAmbiguity {
+
+    static void vaTest (int ... v){
+        System.out.print("vaTest(int ...): "+ "Number of args: "+ v.length+ "Contents: ");
+
+        for (int x : v){
+            System.out.print(x+ " ");
+        }
+        System.out.println();
+    }
+
+    static void vaTest (boolean ... v){
+        System.out.print("vaTest(int ...): "+ "Number of args: "+ v.length+ "Contents: ");
+
+        for (boolean x : v){
+            System.out.print(x+ " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        vaTest(1,2,3);
+        vaTest(true, false);
+        vaTest(); //this is ambiguous method call and hence causes error
+    }
+
+
+
+}
